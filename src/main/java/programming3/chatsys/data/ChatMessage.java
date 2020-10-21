ec4e6bb123d879ea5a19af71977166fe094403b9
jz-1 <1138909620@qq.com>
2020-10-21 20:10:54 +0800

    first commit: create the class chatMessage

--- src/main/java/programming3/chatsys/data/ChatMessage.java
+++ src/main/java/programming3/chatsys/data/ChatMessage.java
@@ -1,6 +1,37 @@
 package programming3.chatsys.data;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class ChatMessage {
 
+    private String message;
+
+    public ChatMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String format(){
+        return null;
+    }
+
+    public void parse(String formatted){
+
+    }
+
+    public void save(File filename) throws Exception{
+
+        FileWriter fileWriter = new FileWriter(filename, true);
+
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        printWriter.println(this.message);
+        printWriter.close();
+    }
 
 }
---
+++ src/test/java/programming3/chatsys/data/ChatMessageTest.java
@@ -0,0 +1,41 @@
+package programming3.chatsys.data;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatMessageTest {
+
+    ChatMessage chatMessage;
+
+    @BeforeEach
+    void setUp() {
+        chatMessage = new ChatMessage("test");
+    }
+
+    @Test
+    void getMessage() {
+        assertEquals(chatMessage.getMessage(), "test");
+    }
+
+    @Test
+    void format() {
+    }
+
+    @Test
+    void parse() {
+    }
+
+    @Test
+    void save(){
+        //chatMessage.save("messages_test.txt");
+    }
+
+    @AfterEach
+    void clean(){
+
+    }
+}
